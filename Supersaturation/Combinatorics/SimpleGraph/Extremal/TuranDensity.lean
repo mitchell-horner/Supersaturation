f3efc311ebcf4486aa5da59aa8dc30d2cc522a8d
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-11-08 19:08:24 +1100

    Use `isGLB_turanDensity`

--- Supersaturation/Combinatorics/SimpleGraph/Extremal/TuranDensity.lean
+++ Supersaturation/Combinatorics/SimpleGraph/Extremal/TuranDensity.lean
@@ -9,27 +9,26 @@ variable {V W : Type*} {G : SimpleGraph V} {H : SimpleGraph W}
 
 -- TODO https://github.com/leanprover-community/mathlib4/pull/29449/files
 theorem isContained_of_card_edgeFinset (H : SimpleGraph W) {ε : ℝ} (hε_pos : 0 < ε) :
-    ∃ N, ∀ {V : Type*} [Fintype V], N ≤ card V →
-      ∀ {G : SimpleGraph V} [DecidableRel G.Adj],
-        #G.edgeFinset ≥ (turanDensity H + ε) * (card V).choose 2 → H ⊑ G := sorry
+    ∃ N, ∀ n ≥ N, ∀ {G : SimpleGraph (Fin n)} [DecidableRel G.Adj],
+      #G.edgeFinset ≥ (turanDensity H + ε) * n.choose 2 → H ⊑ G := sorry
 
 -- TODO https://github.com/leanprover-community/mathlib4/pull/29449/files
-theorem turanDensity_eq_sInf (H : SimpleGraph W) :
+theorem turanDensity_eq_csInf (H : SimpleGraph W) :
     turanDensity H = sInf { (extremalNumber n H / n.choose 2 : ℝ) | n ∈ Set.Ici 2 } := sorry
 
 -- TODO https://github.com/leanprover-community/mathlib4/pull/29449/files
-lemma bbdBelow_extremalNumber_div_choose_two (H : SimpleGraph W) :
-    BddBelow { (extremalNumber n H / n.choose 2 : ℝ) | n ∈ Set.Ici 2 } := sorry
+theorem isGLB_turanDensity (H : SimpleGraph W) :
+    IsGLB { (extremalNumber n H / n.choose 2 : ℝ) | n ∈ Set.Ici 2 } (turanDensity H) := sorry
 
 theorem turanDensity_le_extremalNumber_div_choose_two (H : SimpleGraph W) {n : ℕ} (hn : n ≥ 2) :
     turanDensity H ≤ extremalNumber n H / n.choose 2 := by
-  rw [turanDensity_eq_sInf H]
-  exact csInf_le (bbdBelow_extremalNumber_div_choose_two H) ⟨n, hn, rfl⟩
+  rw [turanDensity_eq_csInf H]
+  exact csInf_le (isGLB_turanDensity H).bddBelow ⟨n, hn, rfl⟩
 
 /-- The Turán density of a simple graph is at most one. -/
 theorem turanDensity_le_one (H : SimpleGraph W) : turanDensity H ≤ 1 := by
-  rw [turanDensity_eq_sInf]
-  apply csInf_le_of_le (bbdBelow_extremalNumber_div_choose_two H) ⟨2, le_refl 2, rfl⟩
+  rw [turanDensity_eq_csInf]
+  apply csInf_le_of_le (isGLB_turanDensity H).bddBelow ⟨2, le_refl 2, rfl⟩
   rw [div_le_iff₀ (mod_cast Nat.choose_pos le_rfl), one_mul, Nat.cast_le]
   exact extremalNumber_le_choose_two
 
